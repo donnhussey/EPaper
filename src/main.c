75eba295b5f6861e751ccc0547ece627f0ac8e02
Donn <donn.hussey@gmail.com>
2020-05-13 22:19:36 -0400

    copying from ex

--- src/main.c
+++ src/main.c
@@ -40,30 +40,30 @@ int main(int c, char **v)
     }
 
     printf(text);
-
-    //initialize the display
-    EPD_2IN13_V2_Init(EPD_2IN13_V2_FULL);
-    EPD_2IN13_V2_Clear();
-
-    //Create a new image cache
-    UBYTE *BlackImage;
-    UWORD Imagesize = ((EPD_2IN13_V2_WIDTH % 8 == 0)? (EPD_2IN13_V2_WIDTH / 8 ): (EPD_2IN13_V2_WIDTH / 8 + 1)) * EPD_2IN13_V2_HEIGHT;
-    if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-        printf("cannot allocate mempry, fatal error...\r\n");
+    printf("EPD_2IN13_V2_test Demo\r\n");
+    if(DEV_Module_Init()!=0){
         return -1;
     }
 
-    //paint white to the new image
-    Paint_NewImage(BlackImage, EPD_2IN13_V2_WIDTH, EPD_2IN13_V2_HEIGHT, 270, WHITE);
-    Paint_SelectImage(BlackImage);
-    Paint_SetMirroring(MIRROR_HORIZONTAL);
-    Paint_Clear(WHITE);
-    EPD_2IN13_V2_Display(BlackImage);
+    printf("e-Paper Init and Clear...\r\n");
+    EPD_2IN13_V2_Init(EPD_2IN13_V2_FULL);
+    EPD_2IN13_V2_Clear();
+    DEV_Delay_ms(500);
 
-    Paint_DrawString_EN(140, 15, text, &Font16, BLACK, WHITE);
+    //Create a new image cache
+    UBYTE *BlackImage;
+    /* you have to edit the startup_stm32fxxx.s file and set a big enough heap size */
+    UWORD Imagesize = ((EPD_2IN13_V2_WIDTH % 8 == 0)? (EPD_2IN13_V2_WIDTH / 8 ): (EPD_2IN13_V2_WIDTH / 8 + 1)) * EPD_2IN13_V2_HEIGHT;    if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
+        printf("Failed to apply for black memory...\r\n");
+        return -1;
+    }
+    printf("Paint_NewImage\r\n");
+    Paint_NewImage(BlackImage, EPD_2IN13_V2_WIDTH, EPD_2IN13_V2_HEIGHT, 270, WHITE);
+
+    printf("attempting to hello world...\r\n");
+    Paint_Clear(WHITE);
+    Paint_SetMirroring(MIRROR_HORIZONTAL);
+    Paint_DrawString_EN(1, 1, "Hello, World!\r\nSecond Line!", &Font8, WHITE, BLACK);
     EPD_2IN13_V2_Display(BlackImage);
-    DEV_Delay_ms(20000);
-    DEV_Module_Exit();
-    free(BlackImage);
-    return 0;
+    DEV_Delay_ms(10000);
 }
