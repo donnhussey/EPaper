629affacddfd24150f19a2b407d52e9e29b7fc76
Donn <donn.hussey@gmail.com>
2020-05-14 22:21:53 -0400

    undo refactor

--- src/main.c
+++ src/main.c
@@ -39,7 +39,7 @@ int main(int c, char **v)
     Paint_DrawString_EN(1, 1, text, &font, WHITE, BLACK);
     EPD_2IN13_V2_Display(img_buf);
     */
-   
+
     WriteInput(&img_buf, &text, &font);
 
     free(img_buf);
@@ -58,13 +58,22 @@ void GetInput(int max_lines, int max_length, char* inputBuffer)
 
 void WriteInput(UBYTE *img_buf, char *text, sFONT *font)
 {
+    printf"--0--\n");
     if(DEV_Module_Init()!=0) return -1;
+        printf"--1--\n");
     EPD_2IN13_V2_Init(EPD_2IN13_V2_FULL);
+        printf"--2--\n");
     EPD_2IN13_V2_Clear();
+        printf"--3--\n");
     Paint_NewImage(img_buf, EPD_2IN13_V2_WIDTH, EPD_2IN13_V2_HEIGHT, 270, WHITE);
+        printf"--4--\n");
     Paint_SelectImage(img_buf);
+        printf"--5--\n");
     Paint_SetMirroring(MIRROR_HORIZONTAL);
+        printf"--6--\n");
     Paint_Clear(WHITE);
+        printf"--7--\n");
     Paint_DrawString_EN(1, 1, text, &font, WHITE, BLACK);
+        printf"--8--\n");
     EPD_2IN13_V2_Display(img_buf);
 }
