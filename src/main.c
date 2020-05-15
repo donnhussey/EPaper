244f5748a1e254416865b6f9fb678861ec0a15d7
Donn <donn.hussey@gmail.com>
2020-05-14 22:02:37 -0400

    undo refactor

--- src/main.c
+++ src/main.c
@@ -27,26 +27,16 @@ int main(int c, char **v)
     }
     
     GetInput(max_lines, max_length, &text);
-    //WriteInput(&img_buf, &text, &font);
-    printf(text);
-    printf("----------------------0-------------------------------\n");
-    if(DEV_Module_Init()!=0)  return -1;
-    printf("----------------------1-------------------------------\n");
-    EPD_2IN13_V2_Init(EPD_2IN13_V2_FULL);
-    printf("----------------------2-------------------------------\n");
-    EPD_2IN13_V2_Clear();
-    printf("----------------------3-------------------------------\n");
+
+    printf("Paint_NewImage\r\n");
     Paint_NewImage(img_buf, EPD_2IN13_V2_WIDTH, EPD_2IN13_V2_HEIGHT, 270, WHITE);
-    printf("----------------------4-------------------------------\n");
     Paint_SelectImage(img_buf);
+    Paint_SetMirroring(MIRROR_HORIZONTAL); //
     Paint_Clear(WHITE);
-    Paint_SetMirroring(MIRROR_HORIZONTAL);
-    printf("----------------------5-------------------------------\n");
-    Paint_DrawPage(1, 1, text, font, WHITE, BLACK);
+    Paint_DrawString_EN(140, 15, "waveshare", &Font12, BLACK, WHITE);
     EPD_2IN13_V2_Display(img_buf);
 
     free(img_buf);
-
     return 0;
 }
 
