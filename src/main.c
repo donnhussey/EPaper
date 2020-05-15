6dd8455dce50eac89b76c797fa30eb5243f133a5
Donn <donn.hussey@gmail.com>
2020-05-14 21:41:54 -0400

    break out the drawing routine

--- src/main.c
+++ src/main.c
@@ -45,17 +45,18 @@ void GetInput(int max_lines, int max_length, char* inputBuffer)
     printf(inputBuffer);
 }
 
-void WriteInput(UBYTE img_buf, char *text, sFONT *font)
+void WriteInput(UBYTE *img_buf, char *text, sFONT *font)
 {
+    printf("----------------------0-------------------------------");
     if(DEV_Module_Init()!=0)  return -1;
     EPD_2IN13_V2_Init(EPD_2IN13_V2_FULL);
     EPD_2IN13_V2_Clear();
-    printf("----------------------0-------------------------------");
-    Paint_NewImage(&img_buf, EPD_2IN13_V2_WIDTH, EPD_2IN13_V2_HEIGHT, 270, WHITE);
     printf("----------------------1-------------------------------");
+    Paint_NewImage(&img_buf, EPD_2IN13_V2_WIDTH, EPD_2IN13_V2_HEIGHT, 270, WHITE);
+    printf("----------------------2-------------------------------");
     Paint_Clear(WHITE);
     Paint_SetMirroring(MIRROR_HORIZONTAL);
-    printf("----------------------2-------------------------------");
+    printf("----------------------3-------------------------------");
     Paint_DrawPage(1, 1, text, font, WHITE, BLACK);
     EPD_2IN13_V2_Display(img_buf);
 }
