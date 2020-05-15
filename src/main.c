a4d02ff63d5b79dae66030c21bfe58d800541942
Donn <donn.hussey@gmail.com>
2020-05-14 20:32:53 -0400

    add print, odd behavior...

--- src/main.c
+++ src/main.c
@@ -46,5 +46,5 @@ void GetInput(int max_lines, int max_length, char* inputBuffer)
     char line_buf[max_length];
     for(int i = 0; i < max_lines && fgets(line_buf, max_length, stdin) != NULL; i++)
         inputBuffer = strcat(inputBuffer, strdup(line_buf));
-    printf(inputbuffer);
+    printf(inputBuffer);
 }
