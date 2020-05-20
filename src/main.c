61a6badcf3cbb43cd256ca63bcbe814408c8ee3b
donn <donn.hussey@gmail.com>
2020-05-19 21:08:05 -0400

    fixing NextLine

--- src/main.c
+++ src/main.c
@@ -10,12 +10,12 @@ void  Handler(int signo)
 
 int main(int c, char **v)
 {
-    //signal(SIGINT, Handler); 
+    signal(SIGINT, Handler); 
     
     sFONT font = Font12;
     int max_page_count = 20;
-    int max_line_length = (int)(EPD_2IN13_V2_WIDTH / font.Width) ;
-    int max_lines = (int)(EPD_2IN13_V2_HEIGHT / font.Height);  
+    int max_line_length = (int)(EPD_2IN13_V2_HEIGHT / font.Width); //these are backwards - constants are for portrait mode
+    int max_lines = (int)(EPD_2IN13_V2_WIDTH / font.Height);  
     int total_size = max_page_count * max_line_length * max_lines;
     int page_position = 0;
     int page_count = 0;
@@ -25,21 +25,19 @@ int main(int c, char **v)
     UBYTE *pages[max_page_count];
 
     strcpy(text, "");
-    strcpy(page, "");
 
     GetInput(total_size, text);
 
     int offset = 0;
-    
-
-
-    
+        
     do{
+        strcpy(page, "");
         for(page_position = 0; page_position < max_lines && offset != -1; page_position++){
                 offset = GetNextLine(next_line, text, offset, max_line_length);
                 strcat(page, next_line);
         }
         pages[page_count++] = Render(page, &font);
+        printf("--page %i rendered-- \n%s\n", page_count, page);
     }while(offset != -1);
 
     Display(pages, page_count);
@@ -73,7 +71,7 @@ int GetNextLine(char output[], char input[], int input_offset, int max_line_leng
 {
     int count = 0;
 
-    while(input[count + input_offset] != '\n' && input[count + input_offset] != '\0' && count < max_line_length-2)
+    while(input[count + input_offset] != '\n' && input[count + input_offset] != '\0' && count < max_line_length-3)
     {
         output[count] = input[input_offset + count];
         count++;
